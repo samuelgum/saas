97de11f445f01fd64eec12269d6f9c17fb7ea616
sammycatz <samuelagum@gmail.com>
2015-08-28 18:40:08 +0000

    Fixed Environment for Mailer spelling

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.sntp_settings = {
+ActionMailer::Base.smtp_settings = {
   :address => 'sntp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
