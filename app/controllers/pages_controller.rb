6f6db065906d4a0a082356356a00fb15f3c4a7f8
sammycatz <samuelagum@gmail.com>
2015-09-28 14:02:04 +0000

    Added plans to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
     def home
-    #   @basic_pay_plan = Pay_plan.find(1)  
-    #   @pro_pay_plan = Pay_plan.find(2)  
+      @basic_pay_plan = Pay_plan.find(1)  
+      @pro_pay_plan = Pay_plan.find(2)  
     end
     
     def about
